c8946a073d2ec5a04f1162ecb7275df16d3c7268
Mehmet <mlalek@gmail.com>
2021-08-30 00:39:39 +0200

    update

--- src/test/java/gorest/tests/GetTechPro02.java
+++ src/test/java/gorest/tests/GetTechPro02.java
@@ -3,6 +3,7 @@ package gorest.tests;
 import gorest.utilities.TestBase;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,20 +12,25 @@ import static io.restassured.RestAssured.given;
 public class GetTechPro02 extends TestBase {
 
     Response response;
-    @Test //Assert that "userid" should be 7, title should be " ...", completed should be false
+    @Test // Assert that; "Server" in Headers should be "cloudflare",
+         // "userid" should be 7,
+         // title should be " esse et quis iste est earum aut impedit",
+         // completed should be false
     public void get01() {
-       //spec01.pathParam("id", 123);
+       spec01.pathParam("id", 123);
         response = given().
                 spec(spec01).
                 when().
-                get();
+                get("/{id}");
         response.prettyPrint();
+        response.prettyPeek();
 
         response.
                 then().
                 assertThat().
-                statusCode(200).
-                contentType(ContentType.JSON);
+                statusCode(200);
+        Assert.assertEquals(response.getHeader("Server"), "cloudflare");
+
     }
 }
 
--- src/test/java/gorest/utilities/TestBase.java
+++ src/test/java/gorest/utilities/TestBase.java
@@ -2,11 +2,13 @@ package gorest.utilities;
 
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
     protected RequestSpecification spec01;
 
-    public void spec01(){
+    @BeforeMethod
+    public void setUp(){
         spec01 = new RequestSpecBuilder().
                 setBaseUri("https://jsonplaceholder.typicode.com/todos").
                 build();
