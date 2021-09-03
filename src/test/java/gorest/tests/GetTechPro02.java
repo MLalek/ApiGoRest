b41c3c0f2d7689e2e55f99dc9fb6d8ed4ac419c3
Mehmet <mlalek@gmail.com>
2021-09-03 21:46:43 +0200

    updated

--- src/test/java/gorest/tests/GetTechPro02.java
+++ src/test/java/gorest/tests/GetTechPro02.java
@@ -2,6 +2,7 @@ package gorest.tests;
 
 import gorest.utilities.TestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,7 +18,8 @@ public class GetTechPro02 extends TestBase {
          // title should be " esse et quis iste est earum aut impedit",
          // completed should be false
 
-    @Test // burada path i kullandik
+    @Test // burada path i kullandik  /* JSONPATH: is used to navigate in Json data
+    //for multiple data use query param
     public void get01() {
        spec01.pathParam("id", 123); //id si 123 olani cagir
         response = given().
@@ -27,18 +29,32 @@ public class GetTechPro02 extends TestBase {
         response.prettyPrint();
         //response.prettyPeek();
 
+        //1 task
+        JsonPath json = response.jsonPath();
+
+        System.out.println(json.getString("firstname")); //"Susan"
+        System.out.println(json.getInt("total price")); //123,   getInt because you call int / number
+        System.out.println(json.getBoolean("depositpaid")); //false, get boolean
+        System.out.println(json.getString("bookingdates.checkin")); //"2020-12-13"
+
+        //2 task
         response.
                 then().
                 assertThat().
                 statusCode(200);
+
         Assert.assertEquals(response.getHeader("Server"), "cloudflare");
 
     }
 
 
-    @Test // burada query param i kullandik
+    @Test // burada query param i kullandik.  /* Query param is use to filter*/ syntax is  ?key=value / "&" is to multiple data
+          // birden fazla degeri cagirdik
     public void get02(){
-        spec02.queryParam("firstname", "Jim"); // Isme/firstname  gore ariyoruz
+//        spec02.queryParam("firstname", "Jim"); // Isme/firstname  gore ariyoruz
+//        spec02.queryParam("totalprice", 2);
+        spec02.queryParam("firstname", "Susan", "totalprice",2); // Bu kisa yazilim.
+                                                                                          // To remove repetation use "query param"
         response = given().
                 spec(spec02).
                 when().
@@ -53,6 +69,8 @@ public class GetTechPro02 extends TestBase {
                 get("/10"); // burda yukarda sorguladigimizda 10 verince 10 u cagirip assert etmis  oluyoruz ve tum liste geliyor
                                 // print:10 numarali id nin tum bilgileri geliyor
         response.prettyPrint();
+
+        Assert.assertTrue(response.asString().contains("Susan"));
 
     }
 }
